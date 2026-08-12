346e6e32fa9dd2509bb005273b311ddc172ef4f3
Divyanshu0902 <divyanshu.kumar.it28@heritageit.edu>
2026-08-12 17:04:08 +0530

    finally mastered json API handling and rendering in the UI

--- API-handling3/script.js
+++ API-handling3/script.js
@@ -2,7 +2,28 @@ const url = "https://jsonplaceholder.typicode.com/posts";
 
 async function getAPIdata(){
     const response = await fetch(url);
-    const jsValues = response.json();
+    const cards = await response.json();
 
-    console.log(jsValues);
+    const cardsArr = cards.map((card) => {
+        return `
+            <article class="post-card">
+                <div class="post-meta">
+                    <span class="post-id">#${card.id}</span>
+                    <span class="user-id">#${card.userId}</span>
+                </div>
+
+                <h2>${card.title}</h2>
+
+                <p>${card.body}</p>
+            </article>
+        `;
+    })
+
+    const cardsHTML = cardsArr.join("\n");
+
+    const postsContainer = document.querySelector(".section");
+    postsContainer.innerHTML = cardsHTML;
+
 }
+
+getAPIdata();
